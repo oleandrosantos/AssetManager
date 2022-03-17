d38d700182edb64df38f8668941b2f47114c6b68
Leandro Santos <contato@oleandrosantos.me>
2022-03-17 01:39:48 -0300

    feat: implementado AutoMapps

---
+++ Profile/UserProfile.cs
@@ -0,0 +1,15 @@
+
+using AssetManager.Model;
+using AssetManager.ViewModel;
+
+namespace AssetManager.Profile;
+
+public class UserProfile :AutoMapper.Profile
+{
+    public UserProfile()
+    {
+        CreateMap<CreateUserViewModel, UserModel>();
+        CreateMap<UserModel,CreateUserViewModel>();
+    }
+    
+}
