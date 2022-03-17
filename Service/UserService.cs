b884c89be3429fa3e3d7a6a0f896364d9b8a703f
Leandro Santos <contato@oleandrosantos.me>
2022-03-17 01:40:16 -0300

    feat: user service, implementado cadastro de usuario

--- Service/UserService.cs
+++ Service/UserService.cs
@@ -1,21 +1,36 @@
 using AssetManager.Data;
 using AssetManager.Model;
+using AssetManager.Profile;
+using AssetManager.ViewModel;
+using AutoMapper;
+using System.Security.Cryptography;
 
 namespace AssetManager.Service;
 
-public class UserService
+public class UserService :IUserService
 {
-    private readonly DataContext _context;
+    private DataContext _context;
+    private IMapper _mapper;
+    private IUserService _userServiceImplementation;
 
-    public UserService(DataContext context)
+    public UserService(DataContext context, IMapper mapper)
     {
         _context = context;
+        _mapper = mapper;
     }
 
-    public string Create(UserModel newUser)
+    public string Create(CreateUserViewModel dadosUsuario)
     {
-        _context.usuario.Add(newUser);
+        UserModel novoUsuario = _mapper.Map<UserModel>(dadosUsuario);
+        novoUsuario.idUsuario = Guid.NewGuid().ToString("N");
+        novoUsuario.token = novoUsuario.idUsuario;
+        _context.usuario.Add(novoUsuario);
         _context.SaveChanges();
-        return $"usuario {newUser.name} cadastrado com sucesso";
-    } 
+        return $"usuario {novoUsuario.name} cadastrado com sucesso";
+    }
+
+    private string criandoHashDaSenha(string senha)
+    {
+        
+    }
 }
