9f91da8075283b494329d1d821b9aaa7663a6de2
Leandro Santos <contato@oleandrosantos.me>
2022-03-17 01:39:03 -0300

    Inserindo INjecao de Dependencia

--- Program.cs
+++ Program.cs
@@ -1,15 +1,21 @@
+using AssetManager;
 using AssetManager.Data;
+using AssetManager.Profile;
+using AssetManager.Service;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddAutoMapper(typeof(UserProfile));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddTransient<IUserService, UserService>();
 var connectionString = "server=localhost;user=root;password=12345678;database=AssetDB";
 var serverVersion = new MySqlServerVersion(new Version(8, 0, 28));
 
@@ -28,6 +34,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.MapGet("/", () => "Hello World!");
 
 app.UseHttpsRedirection();
 
