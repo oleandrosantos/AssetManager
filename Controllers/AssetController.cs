80efcfe9e7c6296b29aee466c45d1034e6772f12
Leandro Santos <contato@oleandrosantos.me>
2022-03-24 02:20:10 -0300

    feat: update asset controller

--- Controllers/AssetController.cs
+++ Controllers/AssetController.cs
@@ -1,6 +1,39 @@
+using AssetManager.Model;
+using Microsoft.AspNetCore.Mvc;
+
 namespace AssetManager.Controllers;
 
-public class AssetController
+[ApiController]
+[Route("[controller]")]
+public class AssetController : ControllerBase
 {
+    private IAssetService _assetService;
+    public AssetController(IAssetService assetService)
+    {
+        _assetService = assetService;
+    }
+
+    [HttpPost("/Create")]
+    public IActionResult Create(AssetModel asset)
+    {
+        var restultado = _assetService.Create(asset);
+        if (restultado != null)
+        {
+            return Ok(restultado);
+        }
+
+        return BadRequest("Erro, não foi possivel criar o Asset");
+    }
     
+    [HttpPatch("/Update")]
+    public IActionResult Update(AssetModel asset)
+    {    var restultado = _assetService.Update(asset);
+        if (restultado != null)
+        {
+            return Ok(restultado);
+        }
+
+        return BadRequest("Erro, não foi possivel criar o Asset");
+        
+    }
 }
