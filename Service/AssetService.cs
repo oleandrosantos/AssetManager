839c9552001b38bb0b39984b110e587c62fa5413
Leandro Santos <contato@oleandrosantos.me>
2022-03-24 02:19:22 -0300

    feat: implantara assetservice

--- Service/AssetService.cs
+++ Service/AssetService.cs
@@ -1,18 +1,33 @@
+using AssetManager;
 using AssetManager.Model;
 using AssetManager.Repository;
 
 namespace AssetManager.Service;
 
-public class AssetService
+public class AssetService :IAssetService
 {
     private AssetRepository _assetRepository;
     public AssetService(AssetRepository assetRepository)
     {
         _assetRepository = assetRepository;
     }
-    public string createAsset(AssetModel asset)
+    public string Create (AssetModel asset)
     {
         var result = _assetRepository.Create(asset);
-        return result;
+        if (result == null)
+        {
+            throw new Exception("Não foi possivel cadastrar o Asset no banco");
+        }
+        return $"{result.assetName} foi cadastrado com sucesso sob o id = {result.idAsset}";
+    }
+
+    public string Update(AssetModel asset)
+    {
+        var result = _assetRepository.Update(asset);
+        if (result == null)
+        {
+            throw new Exception("Não foi possivel Atualizado o Asset no banco");
+        }
+        return $"{result.assetName} foi Atualizado com sucesso sob o id = {result.idAsset}";
     }
 }
