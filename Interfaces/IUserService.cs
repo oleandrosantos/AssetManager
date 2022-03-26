a093f39363012ff88e44031fc179c142acbba502
Leandro Santos <contato@oleandrosantos.me>
2022-03-26 09:15:55 -0300

    feat: criando metodo buscar por email

--- Interfaces/IUserService.cs
+++ Interfaces/IUserService.cs
@@ -5,6 +5,7 @@ namespace AssetManager;
 
 public interface IUserService
 {
-    public string Create(CreateUserViewModel newUser);
+    public string Create(UserViewModel newUser);
     public LoginResult Login(string email, string password);
+    public UserModel? BuscarPorEmail(string email);
 }
