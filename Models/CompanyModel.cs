f43437a5020f3e91a376f977798d685c13718120
Leandro Santos <contato@oleandrosantos.me>
2022-03-26 09:14:53 -0300

    feat: create company

--- Models/CompanyModel.cs
+++ Models/CompanyModel.cs
@@ -7,7 +7,7 @@ public class CompanyModel
 {
     [Key]
     public int idCompany { get; set; }
-    public string companyName { get; set; }
+    public string? companyName { get; set; }
     [MinLength(14), StringLength(14)]
-    public string cnpj { get; set; }
+    public string? cnpj { get; set; }
 }
