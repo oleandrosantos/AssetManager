32d814ae7f32c6b140aebccc07964674af1ad2f9
Leandro Santos <leandro.santos@consuti.com.br>
2022-03-31 23:53:18 -0300

    feat: create repositorys

--- Repository/CompanyRepository.cs
+++ Repository/CompanyRepository.cs
@@ -1,9 +1,12 @@
 using AssetManager.Data;
+using AssetManager.Interfaces;
 using AssetManager.Model;
+using AssetManager.ViewModel;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AssetManager.Repository;
 
-public class CompanyRepository
+public class CompanyRepository :ICompanyService
 {
     private DataContext _context;
 
@@ -16,5 +19,41 @@ public class CompanyRepository
     {
        return _context.company
            .FirstOrDefault(c => c.idCompany == idCompany);
+    }
+
+    public async Result CreateCompany(CompanyModel company)
+    {
+        if (CompanyExists(company))
+            return new Result(false, $"Esta companhia ja esta cadastrada em nosso sistema!");
+
+        _context.company.Add(company);
+        await _context.SaveChangesAsync();
+        return new Result(true, $"{company.companyName} cadastrada com sucesso");
+    }
+
+    public bool DeleteCompany(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public CompanyModel? ObterCompanyPorId()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool UpdateCompany(int id)
+    {
+        throw new NotImplementedException();
+    }
+    
+    private bool CompanyExists(CompanyModel company)
+    {
+        var DataCompany = _context.company.Where(c => c.cnpj == company.cnpj && c.companyName.ToLower().Trim() == company.companyName.ToLower().Trim())
+            .FirstOrDefault();
+
+        if (DataCompany != null)
+            return false;
+
+        return true;
     }
 }
---
+++ Repository/LocationAssetRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace AssetManager.Repository
+{
+    public class LocationAssetRepository
+    {
+    }
+}
