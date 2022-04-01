e5a17037aa4c62edeb49e2ff39e9ceec2ba58c49
Leandro Santos <leandro.santos@consuti.com.br>
2022-03-31 23:53:08 -0300

    feat: create profiles

---
+++ Profile/AssetProfile.cs
@@ -0,0 +1,15 @@
+﻿using AssetManager.Model;
+using AssetManager.ViewModel;
+
+namespace AssetManager.Profile
+{
+    public class AssetProfile :AutoMapper.Profile
+    {
+        public AssetProfile()
+        {
+            CreateMap<AssetModel, CreateAsset>();
+
+            CreateMap<CreateAsset, AssetModel>();
+        }
+    }
+}
