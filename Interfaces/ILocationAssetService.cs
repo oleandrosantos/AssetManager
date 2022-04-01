35277969e19bba6b453f94b89a90a6ab1b823217
Leandro Santos <leandro.santos@consuti.com.br>
2022-04-01 18:56:25 -0300

    feat: interface location asset service

--- Interfaces/ILocationAssetService.cs
+++ Interfaces/ILocationAssetService.cs
@@ -1,6 +1,13 @@
-﻿namespace AssetManager.Interfaces
+﻿using AssetManager.Model;
+using AssetManager.ViewModel;
+
+namespace AssetManager.Interfaces
 {
-    public class ILocationAssetService
+    public interface ILocationAssetService
     {
+        public Result CreateLocationAsset(CreateLocationAsset locationAsset);
+        public Result DeleteLocationAsset(int id);
+        public Result UpdateLocationAsset(LocationAssetModel locationAssetModel);
+        public List<LocationAssetModel> LocationAssetsList();
     }
 }
