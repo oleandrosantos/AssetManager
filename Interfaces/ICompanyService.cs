c937eec9618c1130fe7c9226f0496e48306c3080
Leandro Santos <leandro.santos@consuti.com.br>
2022-03-31 23:51:46 -0300

    feat: create interfaces

---
+++ Interfaces/ICompanyService.cs
@@ -0,0 +1,14 @@
+﻿using AssetManager.Model;
+using AssetManager.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AssetManager.Interfaces
+{
+    public interface ICompanyService
+    {
+        public CompanyModel? ObterCompanyPorId();
+        public Result CreateCompany(CompanyModel company);
+        public bool UpdateCompany(int id);
+        public bool DeleteCompany(int id);
+    }
+}
---
+++ Interfaces/ILocationAssetService.cs
@@ -0,0 +1,6 @@
+﻿namespace AssetManager.Interfaces
+{
+    public class ILocationAssetService
+    {
+    }
+}
