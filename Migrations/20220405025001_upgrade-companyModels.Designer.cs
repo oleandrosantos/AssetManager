9e44f87437a119fce3ddd9d6ab8513a6e0e647d9
Leandro Santos <contato@oleandrosantos.me>
2022-04-08 09:05:24 -0300

    feat: upgrade migrations

--- Migrations/20220405025001_upgrade-companyModels.Designer.cs
+++ Migrations/20220405025001_upgrade-companyModels.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using AssetManager.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AssetManager.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20220405025001_upgrade-companyModels")]
+    partial class upgradecompanyModels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
