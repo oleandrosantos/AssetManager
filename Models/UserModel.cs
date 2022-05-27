54277cfdab0222f92714786786ce204e0c2bf41f
Leandro Santos <leandro.santos@consuti.com.br>
2022-05-27 13:58:21 -0300

    feat: atualizar e revogar acesso do usuario.

--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -6,17 +7,19 @@ namespace AssetManager.Model;
 [Table("tb_usuario")]
 public class UserModel
 {
-    [Key]
-    [MaxLength(32)]
-    public string idUsuario { get; set; }
-    [Required(ErrorMessage = "Nome nao informado")]
-    public string name { get; set; }
-    [Required(ErrorMessage = "Email nao informado")]
-    [MaxLength(256)]
-    public string email { get; set; }
-    [Required(ErrorMessage = "Senha nao informada")]
-    public string password { get; set; }
-    public string token { get; set; }
-    public string role { get; set; }
-    public CompanyModel company { get; set; }
+  [Key]
+  [MaxLength(32)]
+  public string idUsuario { get; set; }
+  [Required(ErrorMessage = "Nome nao informado")]
+  public string name { get; set; }
+  [Required(ErrorMessage = "Email nao informado")]
+  [MaxLength(256)]
+  public string email { get; set; }
+  [Required(ErrorMessage = "Senha nao informada")]
+  public string password { get; set; }
+  public string token { get; set; }
+  public string role { get; set; }
+  public CompanyModel company { get; set; }
+  [DefaultValue(true)]
+  public bool isActive { get; set; }
 }
