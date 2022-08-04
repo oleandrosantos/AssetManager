6805519e67f0d6cb21fb301ca56eb049bb5032f2
Leandro Santos <leandrooliveira574@gmail.com>
2022-08-03 22:17:09 -0300

    feat: adicionando migrations

---
+++ Migrations/20220804000716_init.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AssetManager.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "tb_company",
+                columns: table => new
+                {
+                    IdCompany = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    CompanyName = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Cnpj = table.Column<string>(type: "varchar(14)", maxLength: 14, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    IsAtiva = table.Column<bool>(type: "tinyint(1)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tb_company", x => x.IdCompany);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "tb_asset",
+                columns: table => new
+                {
+                    IdAsset = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    AssetName = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    DepreciationTaxInCents = table.Column<int>(type: "int", nullable: true),
+                    AssetPriceInCents = table.Column<ulong>(type: "bigint unsigned", nullable: false),
+                    AcquisitionDate = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    ExclusionDate = table.Column<DateTime>(type: "datetime(6)", nullable: true),
+                    ExclusionInfos = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    IdCompany = table.Column<int>(type: "int", nullable: false),
+                    Status = table.Column<string>(type: "varchar(64)", maxLength: 64, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Decription = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tb_asset", x => x.IdAsset);
+                    table.ForeignKey(
+                        name: "FK_tb_asset_tb_company_IdCompany",
+                        column: x => x.IdCompany,
+                        principalTable: "tb_company",
+                        principalColumn: "IdCompany",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "tb_usuario",
+                columns: table => new
+                {
+                    IdUsuario = table.Column<string>(type: "varchar(32)", maxLength: 32, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Name = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Email = table.Column<string>(type: "varchar(256)", maxLength: 256, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Password = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Token = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Role = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    IdCompany = table.Column<int>(type: "int", nullable: false),
+                    isActive = table.Column<bool>(type: "tinyint(1)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tb_usuario", x => x.IdUsuario);
+                    table.ForeignKey(
+                        name: "FK_tb_usuario_tb_company_IdCompany",
+                        column: x => x.IdCompany,
+                        principalTable: "tb_company",
+                        principalColumn: "IdCompany",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "tb_loanasset",
+                columns: table => new
+                {
+                    IdLoanAsset = table.Column<string>(type: "varchar(32)", maxLength: 32, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    LoanDate = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    DevolutionDate = table.Column<DateTime>(type: "datetime(6)", nullable: true),
+                    Description = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    IdUsuario = table.Column<string>(type: "varchar(32)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    IdAsset = table.Column<int>(type: "int", nullable: false),
+                    IdCompany = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tb_loanasset", x => x.IdLoanAsset);
+                    table.ForeignKey(
+                        name: "FK_tb_loanasset_tb_asset_IdAsset",
+                        column: x => x.IdAsset,
+                        principalTable: "tb_asset",
+                        principalColumn: "IdAsset",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_tb_loanasset_tb_company_IdCompany",
+                        column: x => x.IdCompany,
+                        principalTable: "tb_company",
+                        principalColumn: "IdCompany",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_tb_loanasset_tb_usuario_IdUsuario",
+                        column: x => x.IdUsuario,
+                        principalTable: "tb_usuario",
+                        principalColumn: "IdUsuario",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tb_asset_IdCompany",
+                table: "tb_asset",
+                column: "IdCompany");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tb_loanasset_IdAsset",
+                table: "tb_loanasset",
+                column: "IdAsset");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tb_loanasset_IdCompany",
+                table: "tb_loanasset",
+                column: "IdCompany");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tb_loanasset_IdUsuario",
+                table: "tb_loanasset",
+                column: "IdUsuario");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tb_usuario_IdCompany",
+                table: "tb_usuario",
+                column: "IdCompany");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tb_loanasset");
+
+            migrationBuilder.DropTable(
+                name: "tb_asset");
+
+            migrationBuilder.DropTable(
+                name: "tb_usuario");
+
+            migrationBuilder.DropTable(
+                name: "tb_company");
+        }
+    }
+}
