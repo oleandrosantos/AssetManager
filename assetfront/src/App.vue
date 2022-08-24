7a75531022ae7722a17bed50955bc2f841496beb
Leandro Santos <leandrooliveira574@gmail.com>
2022-08-23 23:54:33 -0300

    feat: adicionando bootstrap

--- assetfront/src/App.vue
+++ assetfront/src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <LoginApp />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LoginApp from './components/LoginApp.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    LoginApp
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
