c47b51c9206c2ae6b3a7006cabe21ad4aab8b4e1
Leandro Santos <leandrooliveira574@gmail.com>
2022-12-10 14:26:21 -0300

    feat: desmenbrando o front

--- assetfront/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import '../public/src/style.css'
-import 'vuetify/styles'
-import { vuetify } from './plugins/vuetify'
-
-createApp(App).use(router).use(vuetify).mount('#app')
